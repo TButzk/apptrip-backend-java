24dca2f69721a5a33060515e12722e881f813b6e
Guilherme Kilian <guikilian@hotmail.com>
2025-10-06 20:05:32 -0300

    initial

---
+++ src/main/java/unisinos/tripverse/TripverseApplication.java
@@ -0,0 +1,13 @@
+package unisinos.tripverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unisinos/tripverse/TripverseApplicationTests.java
@@ -0,0 +1,13 @@
+package unisinos.tripverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
